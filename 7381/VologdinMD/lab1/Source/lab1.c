ed84280b75895858ef80e789c96d33ec9133e1dd
Makkksx <m.vologdin@mail.ru>
2018-09-16 20:06:09 +0300

    More changes to the god of change'

--- 7381/VologdinMD/lab1/Source/lab1.c
+++ 7381/VologdinMD/lab1/Source/lab1.c
@@ -1,35 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#define N 4
 
-void rec(int l, int r, char *arr)
+void rec(int left, int right, char *arr)
 {
-    for (int i=0;i<l-1;i++)
+    for (int i=0;i<left-1;i++)
     	printf("    ");
     printf("call rec\n");
     char temp;
-    if (l==r)
+    if (left==right)
     {
         printf("Result ");
-        for (int i=0;i<N;i++)
-            printf("%c ", arr[i]);
+        for (int i=0;i<right;i++)
+            printf("%c ", arr[i]);                                                     //End of recursion
         printf("\n");
     }
     else
-        for (int i=l-1;i < r;i++)
+        for (int i=left-1;i < right;i++)
         {
-            if (l-1 != i)
+            if (left-1 != i)
             {
-                temp=arr[l-1];
-                arr[l-1]=arr[i];
+                temp=arr[left-1];
+                arr[left-1]=arr[i];
                 arr[i]=temp;
             }
-            rec(l+1,r,arr);
-            if (l-1 != i)
+            rec(left+1,right,arr);
+            if (left-1 != i)
             {
-                temp=arr[l-1];
-                arr[l-1]=arr[i];
+                temp=arr[left-1];
+                arr[left-1]=arr[i];
                 arr[i]=temp;
             }
         }
@@ -38,14 +37,20 @@ void rec(int l, int r, char *arr)
 
 int main()
 {
+    int N;
+    if (scanf("%d",&N) != 1)
+    {
+        printf("Incorrect data\n");                                                     //Data checking
+        return 0;
+    }    
     char arr[N];
     char simv;
     for (int i=0;i<N;i++)
     {
-        scanf("%c",&simv);
+        scanf("%c",&simv);                                                              //Reading characters
         if (!isspace(simv))
         {
-            arr[i] = simv;
+            arr[i] = simv;                                                              //Data checking
             for (int j=0;j<i;j++)
                 if (arr[i]==arr[j])
                 {
@@ -57,6 +62,6 @@ int main()
             i--;
     }
     printf("\n");
-    rec(1,N,arr);
+    rec(1,N,arr);                                                                        //Recursion call
     return 0;
 }
--- 7381/VologdinMD/lab1/runtest.sh
+++ 7381/VologdinMD/lab1/runtest.sh
@@ -1,4 +1,5 @@
 echo "Test1 input: 
+4
 1
 5
 6
@@ -6,16 +7,18 @@ echo "Test1 input:
 ./lab1 < Tests/test1.txt
 
 echo "Test2 input: 
+4
 /
 *
 -
 +"
 ./lab1 < Tests/test2.txt
 
-echo "Test3 input: 1 # @ 9"
+echo "Test3 input: 4 1 # @ 9"
 ./lab1 < Tests/test3.txt
 
 echo "Test4 input: 
+4
 1
 5
 9
@@ -24,5 +27,9 @@ echo "Test4 input:
 4"
 ./lab1 < Tests/test4.txt
 
-echo "Test5 input:  "
+echo "Test5 input:  
+*
+1
+2
+3"
 ./lab1 < Tests/test5.txt
