316bdf55735d3ebbd60920ae42adb1b2400ef384
Makkksx <m.vologdin@mail.ru>
2018-09-27 20:46:56 +0300

    small fix

--- 7381/VologdinMD/lab2/runtest.sh
+++ 7381/VologdinMD/lab2/runtest.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-gcc ./Source/lab2_v2.c ./Source/func.h -o Lab2
+gcc ./Source/lab2_v2.c -o Lab2
+
 echo "Test1 input: ((1,((5,3)(3,5)))(2,((3,1)(1,3))))"
-cat ./Tests/test1.txt
+./Lab2 < ./Tests/test_for_test1.txt >> results.txt
 
 echo "Test2 input: ((3,((2,((5,1)(1,((3,2)(2,3)))))(6,2)))(4,((1,((1,4)(4,1)))(5,1))))"
-cat ./Tests/test2.txt
+./Lab2 < ./Tests/test_for_test2.txt >> results.txt
 
 echo "Test3 input: ((1,((3,((2,2)(2,2)))(2,((2,((1,1)(1,1)))(1,4)))))(2,((6,4)(4,((1,3)(3,1))))))"
-cat ./Tests/test3.txt
+./Lab2 < ./Tests/test_for_test3.txt >> results.txt
 
 echo "Test4 input: ((2,0)(5,0))"
-cat ./Tests/test4.txt
+./Lab2 < ./Tests/test_for_test4.txt >> results.txt
 
 echo "Test5 input:  ((-6,2)(6,8))"
-cat ./Tests/test5.txt
+./Lab2 < ./Tests/test_for_test5.txt >> results.txt
